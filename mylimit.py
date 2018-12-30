15bac505a93cd13ce2b7a7b8aacd8252b38969b6
Laura Carpaciu <mirelacarpaciu@yahoo.com>
2018-12-30 15:44:45 +0200

    create method generate

---
+++ mylimit.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="mirela",
+    passwd="12345678",
+    database="mypython"
+)
+mycursor = mydb.cursor()
+mycursor.execute("SELECT name, team FROM players LIMIT 9 OFFSET 2")
+myresult = mycursor.fetchall()
+
+no_questions = 10
+
+if no_questions >= len(myresult):
+    print("Nu sunt decat " + str(len(myresult)) + " intrebari posibile!")
+
+questions = []
+index_deja_luat = []
+
+index
+for no_questions in range(no_questions + 1):
+while(index_deja_luat.__contains__(index))
+    l = len(myresult)
+print(l)
+res = myresult[5]
+print(res)
+print(res[0])
+print(res[1])
